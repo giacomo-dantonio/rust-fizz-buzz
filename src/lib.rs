a280352faa5b0beed71b3a30fe206b6bea2a542e
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2022-01-30 17:41:11 +0100

    Refactor logic into lib crate and add test

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+
+pub fn fizzbuzz(n: u64) -> Result<Vec<String>> {
+    let result = (1 .. n+1).map(|k| {
+        if k % 15 == 0 {
+            "fizz buz".to_string()
+        } else if k % 5 == 0 {
+            "buz".to_string()
+        } else if k % 3 == 0 {
+            "fizz".to_string()
+        } else {
+            k.to_string()
+        }
+    }).collect();
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_15() {
+        let result = super::fizzbuzz(15);
+        assert!(result.is_ok());
+
+        let result = result.unwrap();
+        assert_eq!(15, result.len());
+
+        for k in 1 .. 16usize {
+            let value = &result[k - 1];
+
+            if k % 15 == 0 {
+                assert_eq!("fizz buz", value);
+            } else if k % 5 == 0 {
+                assert_eq!("buz", value);
+            } else if k % 3 == 0 {
+                assert_eq!("fizz", value);
+            } else {
+                assert_eq!(&k.to_string(), value);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,7 @@
+use crosscard_interview::fizzbuzz;
+
 use anyhow::Result;
 use std::env;
-use std::fmt::Write;
-
-fn fizzbuzz(n: u64) -> Result<Vec<String>> {
-    let mut result = vec![];
-    result.reserve(n as usize);
-
-    for k in 1 .. n+1 {
-        let mut s = String::new();
-        if k % 15 == 0 {
-            write!(s, "fizz buz")?;
-        } else if k % 5 == 0 {
-            write!(s, "buz")?;
-        } else if k % 3 == 0 {
-            write!(s, "fizz")?;
-        } else {
-            write!(s, "{}", k)?;
-        }
-        result.push(s);
-    }
-
-    Ok(result)
-}
 
 fn main() -> Result<()>{
     let args: Vec<String> = env::args().collect();
